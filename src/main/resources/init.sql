a8be1a471401975f5a95a44ab03793efafa3eb43
AnticoreV <sapronovivancore@gmail.com>
2024-01-23 14:32:59 +0100

    Init sql script for postgres

---
+++ src/main/resources/init.sql
@@ -0,0 +1,8 @@
+INSERT INTO task (id, name, description, deadline, is_done) VALUES
+    (1, 'Task 1', 'Description for Task 1', '2024-02-01', true),
+    (2, 'Task 2', 'Description for Task 2', '2024-02-15', true),
+    (3, 'Task 3', 'Description for Task 3', '2024-02-01', false),
+    (4, 'Task 4', 'Description for Task 4', '2024-02-15', true),
+    (5, 'Task 5', 'Description for Task 5', '2024-02-01', false),
+    (6, 'Task 6', 'Description for Task 6', '2024-02-15', false),
+    (7, 'Task 7', 'Description for Task 7', '2024-03-01', false);
