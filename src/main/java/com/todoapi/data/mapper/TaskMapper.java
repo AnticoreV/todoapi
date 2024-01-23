376404991c046ccca4c7c1d772d85c6a953dbe05
AnticoreV <sapronovivancore@gmail.com>
2024-01-23 14:32:24 +0100

    Mapper impl, lombok and mapstruct bind

---
+++ src/main/java/com/todoapi/data/mapper/TaskMapper.java
@@ -0,0 +1,15 @@
+package com.todoapi.data.mapper;
+
+import com.todoapi.data.dto.TaskDto;
+import com.todoapi.data.entity.Task;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "lombok")
+public interface TaskMapper {
+    TaskMapper INSTANCE = Mappers.getMapper(TaskMapper.class);
+    @Mapping(source = "done", target = "isDone")
+    TaskDto toApi(Task source);
+    Task toEntity(TaskDto taskDto);
+}
